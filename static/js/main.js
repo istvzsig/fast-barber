b52aa7d083154cc205385ed71e1e46a58b5e338d
istvzsig <istvanzsigmond@aol.com>
2025-02-06 14:05:43 +0100

    refactor static dir

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,32 @@
 // static/script.js
+
+const DUMMY_BARBER_IMG_URLS = [
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.eMjfMxVlYuzAcXkq7VoPcQAAAA%26pid%3DApi&f=1&ipt=b39635a9b774b53452266eef7c644b34f5e58f2aece28f1b6816f44f33fa961a&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5xurOceVymIXcglDe5ZUvAAAAA%26pid%3DApi&f=1&ipt=3a161bab345bacaf3f5e4ce0f4503fd47d11a0ef8514e5ee1fd95c9da785ed42&ipo=images",
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7ZOVa68CWitlK-8RwCfd-wAAAA%26pid%3DApi&f=1&ipt=51e683838e8db57896289258e8f1e0157bc63a8f457b60f6d2cf6193e95818a7&ipo=images",
+]
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("booking-form");
+    let barberId = null;
+
+    const barberOptions = document.querySelectorAll(".barber-option");
+    barberOptions.forEach((option, i) => {
+        option.style.backgroundImage = `url('${DUMMY_BARBER_IMG_URLS[i]}')`;
+        option.addEventListener("click", function () {
+            barberOptions.forEach(opt => {
+                opt.classList.remove("selected");
+                opt.classList.add("not-selected")
+            });
+            option.classList.add("selected");
+            barberId = this.dataset.barberId;
+        });
+    });
 
     form.addEventListener("submit", async function (event) {
-        event.preventDefault(); // Prevent page reload
+        event.preventDefault();
 
         const username = document.getElementById("username").value;
-        const barberId = document.getElementById("barber").value;
         const appointmentTime = document.getElementById("appointment-time").value;
 
         if (!username || !barberId || !appointmentTime) {
@@ -28,9 +48,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (response.ok) {
             alert("Booking successful!");
-            location.reload(); // Reload the page to show updated bookings
+            location.reload();
         } else {
             alert("Error booking appointment.");
         }
+
+        document.querySelectorAll(".barber-option")
+            .forEach(opt => opt.classList.remove("selected"));
     });
 });
