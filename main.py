ac3b74958524d2e808f913fa145bc2a4289be50d
istvzsig <istvanzsigmond@aol.com>
2025-02-06 12:42:28 +0100

    remove comments and rename function

--- main.py
+++ main.py
@@ -3,22 +3,18 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-# Import the API router
 from api import api_router
 
 app = FastAPI()
 
-# Templates setup
 templates = Jinja2Templates(directory=str(
     Path(__file__).resolve().parent / "templates"))
 
-# Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Include the API routers
 app.include_router(api_router)
 
 
 @app.get("/")
-def home(request: Request):
+def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
